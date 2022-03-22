aef9ae42b10e1adf668c3a0aa16190968826403d
Dwain Robinson <robinson.dwain.developer@gmail.com>
2022-03-21 22:35:45 -0500

    Updating default route endpoints

--- src/routes/appRouter.js
+++ src/routes/appRouter.js
@@ -7,17 +7,15 @@ const { Router } = express;
 const router = Router();
 
 router.get('/video-service/', (_, res) => {
-  res.json({
-    statusCode: 200,
-    message: 'Welcome to Nitrogen Video Manager Service!'
-  });
+  res
+    .status(200)
+    .send({ message: 'Welcome to Nitrogen Video Manager Service!' });
 });
 
 router.get('/video-service/probeCheck', (_, res) => {
-  res.json({
-    statusCode: 200,
-    message: 'Nitrogen Video Manager service up and running!'
-  });
+  res
+    .status(200)
+    .send({ message: 'Nitrogen Video Manager service up and running!' });
 });
 
 export default router;
